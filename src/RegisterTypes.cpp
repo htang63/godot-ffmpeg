0f8688c8b0f90de356eae4ffc221c2dd0a4d308e
henrytang63 <henry.tang@idnerd.com>
2023-04-21 17:56:19 +0800

    fix register

--- src/RegisterTypes.cpp
+++ src/RegisterTypes.cpp
@@ -9,15 +9,27 @@
 
 using namespace godot;
 
-void register_scenes_types() {
-	ClassDB::register_class<FFmpegNode>();
+void initialize_example_module(ModuleInitializationLevel p_level) {
+    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+        return;
+    }
+
+    ClassDB::register_class<FFmpegNode>();
+}
+
+void uninitialize_example_module(ModuleInitializationLevel p_level) {
+    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+        return;
+    }
 }
 
 extern "C" {
-	GDNativeBool GDN_EXPORT init(const GDNativeInterface *p_interface, const GDNativeExtensionClassLibraryPtr p_library, GDNativeInitialization *r_initialization) {
+	GDExtensionBool  GDN_EXPORT init(const GDExtensionInterface  *p_interface, const GDExtensionClassLibraryPtr  p_library, GDExtensionInitialization  *r_initialization) {
 		godot::GDExtensionBinding::InitObject init_obj(p_interface, p_library, r_initialization);
 
-		init_obj.register_scene_initializer(register_scenes_types);
+		init_obj.register_initializer(initialize_example_module);
+		init_obj.register_terminator(uninitialize_example_module);
+		init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
 
 		return init_obj.init();
 	}
--- src/RegisterTypes.h
+++ src/RegisterTypes.h
@@ -1,3 +1,7 @@
-#pragma once
+#ifndef GDEXAMPLE_REGISTER_TYPES_H
+#define GDEXAMPLE_REGISTER_TYPES_H
 
-void register_scenes_types();
+void initialize_example_module();
+void uninitialize_example_module();
+
+#endif // GDEXAMPLE_REGISTER_TYPES_H
