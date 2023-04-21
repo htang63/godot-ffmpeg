0520c173601cedf770bc8293c969e633be26a7f9
henrytang63 <henry.tang@idnerd.com>
2023-04-21 17:47:47 +0800

    fix register types

--- src/RegisterTypes.cpp
+++ src/RegisterTypes.cpp
@@ -2,7 +2,7 @@
 
 #include "FFmpegNode.h"
 
-#include <godot/gdnative_interface.h>
+#include <gdextension_interface.h>
 #include <godot_cpp/core/class_db.hpp>
 #include <godot_cpp/core/defs.hpp>
 #include <godot_cpp/godot.hpp>
