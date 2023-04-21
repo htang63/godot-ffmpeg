944e00acd280ec73ebe18f9886114921d015aebd
henrytang63 <henry.tang@idnerd.com>
2023-04-21 18:26:53 +0800

    fix export

--- src/RegisterTypes.cpp
+++ src/RegisterTypes.cpp
@@ -24,7 +24,8 @@ void uninitialize_ffmpeg_module(ModuleInitializationLevel p_level) {
 }
 
 extern "C" {
-	GDExtensionBool  GDN_EXPORT ffmpeg_library_init(const GDExtensionInterface  *p_interface, const GDExtensionClassLibraryPtr  p_library, GDExtensionInitialization  *r_initialization) {
+	// Initialization.
+	GDExtensionBool GDE_EXPORT ffmpeg_library_init(const GDExtensionInterface  *p_interface, const GDExtensionClassLibraryPtr  p_library, GDExtensionInitialization  *r_initialization) {
 		godot::GDExtensionBinding::InitObject init_obj(p_interface, p_library, r_initialization);
 
 		init_obj.register_initializer(initialize_ffmpeg_module);
