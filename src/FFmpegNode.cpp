bcb89e0ce7209b7ebe547ca73654fe67bd59e2fd
henrytang63 <henry.tang@idnerd.com>
2023-04-25 11:42:51 +0800

    undo assignment

--- src/FFmpegNode.cpp
+++ src/FFmpegNode.cpp
@@ -198,10 +198,10 @@ void FFmpegNode::_process(float delta) {
 					PackedByteArray image_data;
 					image_data.resize(data_size);
 					memcpy(image_data.ptrw(), frame_data, data_size);
-					image = image->create_from_data(width, height, false, Image::FORMAT_RGBA8, image_data);
+					image->create_from_data(width, height, false, Image::FORMAT_RGBA8, image_data);
 
 					if (first_frame) {
-						texture = texture->create_from_image(image);
+						texture->create_from_image(image);
 						first_frame = false;
 					} else {
 						texture->update(image);
