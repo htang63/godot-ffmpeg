586cc9039694d06c490f6236534dc8cacf9fe5cc
henrytang63 <henry.tang@idnerd.com>
2023-05-25 15:00:13 +0800

    fix test project

--- test/main.gd
+++ test/main.gd
@@ -11,12 +11,3 @@ func _ready():
 func _process(delta):
 	texture = ffmpeg.get_video_texture()
 	return
-
-func _input(event):
-	if event.is_action_pressed("ui_left"):
-		ffmpeg.seek(ffmpeg.get_playback_position() - TIME_SKIP)
-	elif event.is_action_pressed("ui_right"):
-		ffmpeg.seek(ffmpeg.get_playback_position() + TIME_SKIP)
-	elif event.is_action_pressed("ui_accept"):
-		ffmpeg.set_paused(not ffmpeg.is_paused())
-	return
